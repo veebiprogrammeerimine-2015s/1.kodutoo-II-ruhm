54ddb98982a66acadbb3842d739b54b49c1d976a
Koit Korela <koitkor@greeny.cs.tlu.ee>
2015-09-11 13:36:37 +0300

    Kodune töö

---
+++ login.php
@@ -0,0 +1,38 @@
+<?php
+	//echo $_POST["email"]
+	
+	//Muutujad 
+	$email_error = "";
+	$password_error = "";
+	
+	//Kontrollin kas keegi vaiutas nuppu
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		if(empty($_POST["email"])){
+			$email_error = "See väli on kohustuslik";
+		}
+		
+		if(empty($_POST["password"])){
+			$password_error = "See väli on kohustuslik";
+		}
+		
+	}
+?>
+
+<html>
+<head>
+	<title>Login page</title>
+</head>
+<body>
+	<h2>Login</h2>
+	<form action="login.php" method="post">
+	<input name="email" type="email" placeholder="E-post"> <?php echo $email_error;?> <br> <br> 
+	<input name="password" type="password" placeholder="Parool"> <?php echo $password_error;?> <br> <br> 
+	<input type="submit" value="Logi sisse"> <br> <br>
+	</form>
+	
+	
+	
+	<h2>Create user</h2>
+</body>
+
+</html>
