f699f545413cfe16e9bc62dbe7fb6d2176772290
Jaagup Siil <jaagsii@greeny.cs.tlu.ee>
2015-09-11 13:49:15 +0300

    login tund

---
+++ login.php
@@ -0,0 +1,44 @@
+<?php
+
+	//ECHO $_POST["email"];
+	
+	//defineerime muutujad
+	$email_error="";
+	$password_error="";
+	
+	//kontrollin kas keegi vajutas nuppu
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		
+		//echo "jah";
+		
+		//kas e-post on tühi
+		if(empty($_POST["email"])){
+			
+			//jah oli tühi
+			$email_error= "See väli on kohustuslik";
+			}
+			
+			//kas parool on tühi
+			if(empty($_POST["password"])){
+				
+				//jah oli tühi
+				$password_error="See väli on kohustuslik";
+			}
+	}
+
+?>
+<html>
+	<head>
+		<title>Login page</title>
+	</head>
+	<body>
+		<h2>Login</h2>
+		<form action="login.php" method="post">
+			<input name"email" type="email" placeholder="E-post"> <?php echo $email_error; ?><br><br>
+			<input name="password" type="password" placeholder="Parool"> <?php echo $password_error; ?><br><br>
+			<input type="submit" value="Logi sisse"> <br><br>
+		<form>
+		<h2>Create user</h2>
+		
+	</body>
+</html>
