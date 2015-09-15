722e9e186709e82fa11a277de35b0adabda2bfa7
Tauno Lainevool <taunlai@greeny.cs.tlu.ee>
2015-09-15 10:56:53 +0300

    Lehekülg

--- login.php
+++ login.php
@@ -30,6 +30,14 @@ $parool_error = "See väli on kohustuslik";
 <title>Login Page </title>
 </head>
 <body>
+<div style="background-color:blue; color:white; padding:20px;">
+<h2>Lehekülg</h2>
+<p> Eeny, Meeny, Miny, Mo,
+Catch a nigger by his toe,
+If he won't work then let him go;
+Skidum, skidee, skidoo.
+ </p>
+</div>
 
 <h2> Login </h2>
 <form action="login.php" method="POST">
@@ -37,6 +45,10 @@ $parool_error = "See väli on kohustuslik";
 <input type="password" placeholder="Parool"><?php echo $parool_error; ?> <br><br>
 <input type="submit" value="Logi sisse" > <br><br>
 </form>
+
+
+
+
 <h2> Create User </h2>
  <h2><a href="http://www.tlu.ee">Already a Member?</a></h2>
 
