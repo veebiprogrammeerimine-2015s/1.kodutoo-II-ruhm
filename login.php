8f548a03de8598a7bac3d5c7108cadeade7d5a2d
Rimo Esko <rimoesk@greeny.cs.tlu.ee>
2015-09-15 20:27:09 +0300

    Added separate error for create

--- login.php
+++ login.php
@@ -5,6 +5,8 @@
 	//defineerime muutujad
 	$email_error = "";
 	$password_error = "";
+	$email_error2 = "";
+	$password_error2 = "";
 	
 	//kontrollin kas keegi vajutas nuppu
 	if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -22,7 +24,18 @@
 			//jah oli tühi
 			$password_error = "See väli on kohustuslik";
 		}
-		
+		//kas create email on tühi
+		if(empty($_POST["email"])){
+			
+			//jah oli tühi
+			$email_error2 = "See väli on kohustuslik";
+		}
+		//kas create password on tühi
+		if(empty($_POST["email"])){
+			
+			//jah oli tühi
+			$password_error2 = "See väli on kohustuslik";
+		}
 	}
 
 
@@ -42,8 +55,8 @@
 	
 	<h2>Create user</h2>
 	<form action="login.php" method="post">
-	<input name="email" type "email" placeholder="E-mail"> <?php echo $email_error?><br><br>
-	<input name="password" type="password" placeholder="password"> <?php echo $password_error?> <br><br>
+	<input name="email" type "email" placeholder="E-mail"> <?php echo $email_error2?><br><br>
+	<input name="password" type="password" placeholder="password"> <?php echo $password_error2?> <br><br>
 	<input type="submit" value="Create"> <br><br>
 	</form>
 </body>
