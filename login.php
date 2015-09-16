9a56b5111af32e6455052e6c90331c0a092e2d97
Koidu Kannela <koidkan@greeny.cs.tlu.ee>
2015-09-16 21:10:09 +0300

    t2iendatud

--- login.php
+++ login.php
@@ -6,6 +6,11 @@
 	$password_error ="";
 	$first_name_error ="";
 	$last_name_error ="";
+	$email1_error ="";
+	$email2_error ="";
+	$password1_error ="";
+	$password2_error ="";
+	
 	
 	// kontrollin, kas keegi vajutas nuppu
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -30,6 +35,22 @@
 			
 			$last_name_error ="See väli on kohustuslik";
 		}
+		if(empty($_POST["email1"])) {
+			
+			$email1_error = "See väli on kohustuslik";
+		}
+		if(empty($_POST["email2"])) {
+			
+			$email2_error = "See väli on kohustuslik";
+		}
+		if(empty($_PASSWORD["password1"])) {
+			
+			$password1_error = "See väli on kohustuslik";
+		}
+		if(empty($_PASSWORD["password2"])) {
+			
+			$password2_error = "See väli on kohustuslik";
+		}
 	}
 	
 
@@ -49,14 +70,15 @@
 	</form>
 	<h2>Create user</h2>
 	<form action="creat user.php" method="post">
-		<input name="first name" type="text" placeholder="Eesnimi" > <?php echo $first_name_error
-		?> <br><br>
+		<input name="first name" type="text" placeholder="Eesnimi" > <?php echo $first_name_error ?> <br><br>
 		<input name="last name" type="text" placeholder="Perekonnanimi"> <?php echo $last_name_error ?> <br><br>
 		<input name="email1" type="email" placeholder="E-post" > <?php echo $email1_error ?> <br><br>
 		<input name="email2" type="email" placeholder="Korda e-posti" > <?php echo $email2_error ?> <br><br>
 		<input name="password1" type="password" placeholder="Parool"> <?php echo $password1_error ?> <br><br>
 		<input name="password2" type="password" placeholder="Korda parooli"> <?php echo $password2_error ?> <br><br>
 		<input type="submit" value="Loo konto"> <br><br>
+	</form>
+	
 	
 	
 	
