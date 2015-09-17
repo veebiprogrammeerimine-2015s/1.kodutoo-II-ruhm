aa52e7ff9d055d7ee97c710682c04ea2003a96db
Hendrik Karilaid <karilaid@greeny.cs.tlu.ee>
2015-09-18 00:29:16 +0300

    added errors

--- login.php
+++ login.php
@@ -4,17 +4,21 @@
 	$password_error = "";
 	$password_fail = "";
 	$password_right = "";
+	$mail_error = "";
+	$pass_error = "";
+	$passc_error = "";
+	
+	
+	
 	
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
-	if ($_POST["password"] == $_POST["confirm_password"]) {
-		$password_right = "(Y)";
+		
+		if ($_POST["password"] == $_POST["confirm_password"]) {
+		$password_right = "Paroolid klapivad";
 }
 	else {
 		$password_fail = "Paroolid ei klapi.";
 }
-	}
-	if($_SERVER["REQUEST_METHOD"] == "POST") {
-		
 		if ( empty($_POST["email"])) {
 			
 			$email_error = "See väli on kohustuslik";
@@ -24,6 +28,24 @@
 		if ( empty($_POST["pass"])) {
 			
 			$password_error = "See väli on kohustuslik";
+			
+			
+		}
+		if ( empty($_POST["mail"])) {
+			
+			$mail_error = "See väli on kohustuslik";
+			
+			
+		}
+		if ( empty($_POST["password"])) {
+			
+			$pass_error = "See väli on kohustuslik";
+			
+			
+		}
+		if ( empty($_POST["confirm_password"])) {
+			
+			$passc_error = "See väli on kohustuslik";
 			
 			
 		}
@@ -43,14 +65,13 @@
 	
 	<h2>Kasutaja loomine</h2>
 	
-	<input name="mail" type="email" placeholder="E-post"> <br><br>
-	<input name="password" type="password" placeholder="Parool"> <br><br>
-	<input name="confirm_password" type="password" placeholder="Korda parooli"> <?php echo $password_fail  ?> <br><br>
+	<input name="mail" type="email" placeholder="E-post"><?php echo $mail_error  ?> <br><br> 
+	<input name="password" type="password" placeholder="Parool"><?php echo $pass_error  ?> <br><br> 
+	<input name="confirm_password" type="password" placeholder="Korda parooli"> <?php echo $password_fail; echo $password_right; echo $passc_error  ?> <br><br>
 	<input name="name" type="name" placeholder="Eesnimi"> <br><br>
 	<input name="lastname" type="name" placeholder="Perekonnanimi"> <br><br>
 	<input type="submit" value="Loo kasutaja"> <br><br>
 	</form>
-
 </body>
 
 
