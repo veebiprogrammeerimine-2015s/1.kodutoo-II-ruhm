b069c679e77520cdb387d52eff5bdb60397d0b78
Hendrik Karilaid <karilaid@greeny.cs.tlu.ee>
2015-09-18 00:08:08 +0300

    added confirm password

--- login.php
+++ login.php
@@ -2,7 +2,17 @@
 
 	$email_error = "";
 	$password_error = "";
+	$password_fail = "";
+	$password_right = "";
 	
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+	if ($_POST["password"] == $_POST["confirm_password"]) {
+		$password_right = "(Y)";
+}
+	else {
+		$password_fail = "Paroolid ei klapi.";
+}
+	}
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
 		
 		if ( empty($_POST["email"])) {
@@ -30,13 +40,16 @@
 	<input name="email" type="email" placeholder="E-post"> <?php echo $email_error  ?> <br><br>
 	<input name="pass" type="password" placeholder="Parool"> <?php echo $password_error  ?><br><br>
 	<input type="submit" value="Logi sisse"> <br><br>
-	</form>
-	<h2>Create user</h2>
+	
+	<h2>Kasutaja loomine</h2>
+	
 	<input name="mail" type="email" placeholder="E-post"> <br><br>
 	<input name="password" type="password" placeholder="Parool"> <br><br>
-	<input name="confirm_password" type="password" placeholder="Korda parooli"> <br><br>
+	<input name="confirm_password" type="password" placeholder="Korda parooli"> <?php echo $password_fail  ?> <br><br>
 	<input name="name" type="name" placeholder="Eesnimi"> <br><br>
 	<input name="lastname" type="name" placeholder="Perekonnanimi"> <br><br>
+	<input type="submit" value="Loo kasutaja"> <br><br>
+	</form>
 
 </body>
 
