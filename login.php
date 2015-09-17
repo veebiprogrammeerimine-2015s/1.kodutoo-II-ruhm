8efd0438fa190c6c7d0f18fca3b1481db2d9dbd5
Vitali Maksimov <vitamak@greeny.cs.tlu.ee>
2015-09-17 23:28:56 +0300

    pass

--- login.php
+++ login.php
@@ -1,51 +1,47 @@
 <?php
 	
-	//echo $_POST["email"];
+	// LOGIN.PHP
 	
-	//defineerime muutujad
-	
+	// errori muutujad peavad igal juhul olemas olema 
 	$email_error = "";
-	$pass_error = "";
+	$password_error = "";
 	
-	//kontrollin kas keegi vajutas nuppu
+	// kontrollime et keegi vajutas input nuppu
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
 		
-		//echo "jah";
+		//echo "ktonibuty nazhimal knopku";
 		
-		//kas e-post on tühi
-	if( empty($_POST["email"]) ) {
+		//proverka pochty pustoy ili net
+		if ( empty($_POST["email"]) ) {
+			$email_error = "must have be";
+		}
 		
-		//jah oli tühi
-		$email_error = "must be";
+		//proverka porolya, pustoi ili net
+		if ( empty($_POST["password"]) ) {
+			$password_error = "must have be";
+		} else {
+			
+			
+		}
+		
+	}
 	
-
-			//kas pass on tühi
-	if( empty($_POST["pass"]) ) {
-		
-		//jah oli tühi
-		$pass_error = "must be";
-	}
-	}
-	}
-
-
 ?>
-
 <html>
 <head>
-	<title>Login Page</title>
+	<title>Login page</title>
 </head>
 <body>
-	siia tuleb login
-	<h2>Login</h2>
-	<form ation="login.php" method="post":>
-	<input name="email" type="email" placeholder="e-mail" > <?php echo $email_error ?> <br><br>
-	<input name="pass" type="pass" placeholder="parool"> <br><br> 
-	<input type="submit" value="log in" > <br><br> 
-	</form>
+	
+	<h2>Log in</h2>
+		
+		<form action="login.php" method="post" >
+			<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?><br><br>
+			<input name="password" type="password" placeholder="Parool"> <?php echo $password_error; ?> <br><br>
+			<input type="submit" value="Log in">
+		</form>
 	
 	<h2>Create user</h2>
-
 	
 </body>
 </html>
