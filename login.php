0e082b111bfd7cd79386ec70f147f7e709e4b473
Jan-Erik Ilves <janilv@greeny.cs.tlu.ee>
2015-09-17 17:10:35 +0300

    probleemid puttyga, ei saand mitu korda commitida

--- login.php
+++ login.php
@@ -20,8 +20,19 @@
 	}
 
 	// kas parool on tühi
+			if ( empty($_POST["password"]) ) {
+			$password_error = "See väli on kohustuslik";
+		} else {
+			
+			//Parool pole tühi
+			if(strlen($_POST["password"]) < 6) {
+				
+				$password_error = "Parool peab olema vähemalt 6 tähemärki pikk.";
+			}		
+		}
+	}
 	
-}	
+	
 	
 ?>
 <html>
@@ -30,7 +41,7 @@
 </head>
 <body>
 
-	<h2>Login</h2>
+	<h2>Logi sisse</h2>
 	<form action="login.php" method="post">
 	<input name="email" type="email" placeholder="E-post" > <?php echo $email_error ?>	<br><br>
 	<input name="password" type="password" placeholder="Parool" > <?php echo $password_error ?>	<br><br>
@@ -38,7 +49,18 @@
 	</form>
 	
 	
-	<h2>Create user</h2>
-	
+	<h2>Tee kasutaja</h2>
+	<form action="login.php" method="post" >
+	<input name="email" type="email" placeholder="Email ">*<?php echo $email_error ?><br><br>
+	<input name="pass" type="password" placeholder="Parool ">*<?php echo $password_error ?><br><br>
+	<input name="name" type="first_name" placeholder="Eesnimi">*<br><br>
+	<input name="name" type="last_name" placeholder="Perekonnanimi">*<br><br>
+	<input name="date" type="age" placeholder="Vanus">*<br><br>
+	<input type="submit" value="Registeeri">
+	</form>
+	MVP idee
+	Idee seisneb selles, et inimesed, kes käivad jõusaalis, saaks mugavalt oma järgida oma toitumist.
+	 Koostaks kalkulaatori(vanus, rasvaprotsent, kui palju liigutakse nädalas) ja vastavalt oma soovidele(kaalu kaotus/massi suurendamine) näeks inimene, kuidas toiteväärtust muuta ja kuidas õigesti süüa.
+	 Et asja natuke omamoodi luua, siis võiks olla lisa võimalus, et koostab päeva ja hiljem nädalaplaani ja lisab sellele kõrvale retseptid, et poest lihtsam asju osta oleks ja saaks täpselt toitumisnõuded ära täita.
 </body>
 </html>
