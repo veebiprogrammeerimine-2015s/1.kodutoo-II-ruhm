3c3b2c7c43f332663298a90eeb97d35d1ea84c66
Jan-Erik Ilves <janilv@greeny.cs.tlu.ee>
2015-09-25 12:45:47 +0300

    Added Create User validation

--- login.php
+++ login.php
@@ -5,6 +5,9 @@
 	//Defineerime muutuja
 	$email_error ="";
 	$password_error = "";
+	$first_name_error = "";
+	$last_name_error = "";
+	$date_error = "";
 	
 	//kontrollin, kas keegi vajutas nuppu
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -17,10 +20,10 @@
 			//jah oli tühi
 			$email_error = "See väli on kohustuslik";
 	
-	}
+		}
 
 	// kas parool on tühi
-			if ( empty($_POST["password"]) ) {
+		if ( empty($_POST["password"]) ) {
 			$password_error = "See väli on kohustuslik";
 		} else {
 			
@@ -30,6 +33,35 @@
 				$password_error = "Parool peab olema vähemalt 6 tähemärki pikk.";
 			}		
 		}
+		
+		
+		if ( empty($_POST["first_name"])	) {
+			
+			//jah oli tühi
+			$first_name_error = "See väli on kohustuslik";
+	
+		}
+		
+		if ( empty($_POST["last_name"])	) {
+			
+			//jah oli tühi
+			$last_name_error = "See väli on kohustuslik";
+	
+		}
+		
+		if ( empty($_POST["last_name"])	) {
+			
+			//jah oli tühi
+			$date_error = "See väli on kohustuslik";
+	
+		}
+		
+		
+		
+		
+		
+		
+		
 	}
 	
 	
@@ -51,14 +83,15 @@
 	
 	<h2>Tee kasutaja</h2>
 	<form action="login.php" method="post" >
-	<input name="email" type="email" placeholder="Email ">*<?php echo $email_error ?><br><br>
-	<input name="pass" type="password" placeholder="Parool ">*<?php echo $password_error ?><br><br>
-	<input name="name" type="first_name" placeholder="Eesnimi">*<br><br>
-	<input name="name" type="last_name" placeholder="Perekonnanimi">*<br><br>
-	<input name="date" type="age" placeholder="Vanus">*<br><br>
+	<input name="cemail" type="email" placeholder="Email ">*<?php echo $cemail_error ?><br><br>
+	<input name="cpass" type="password" placeholder="Parool ">*<?php echo $cpassword_error ?><br><br>
+	<input name="first_name" type="text" placeholder="Eesnimi">*<?php echo $first_name_error ?><br><br>
+	<input name="last_name" type="name" placeholder="Perekonnanimi">*<?php echo $last_name_error ?><br><br>
+	<input name="date" type="age" placeholder="Vanus">*<?php echo $date_error ?><br><br>
 	<input type="submit" value="Registeeri">
 	</form>
 	MVP idee
+	
 	Idee seisneb selles, et inimesed, kes käivad jõusaalis, saaks mugavalt oma järgida oma toitumist.
 	 Koostaks kalkulaatori(vanus, rasvaprotsent, kui palju liigutakse nädalas) ja vastavalt oma soovidele(kaalu kaotus/massi suurendamine) näeks inimene, kuidas toiteväärtust muuta ja kuidas õigesti süüa.
 	 Et asja natuke omamoodi luua, siis võiks olla lisa võimalus, et koostab päeva ja hiljem nädalaplaani ja lisab sellele kõrvale retseptid, et poest lihtsam asju osta oleks ja saaks täpselt toitumisnõuded ära täita.
