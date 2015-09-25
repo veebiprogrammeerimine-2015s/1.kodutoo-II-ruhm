6a49775b2d2323e7a6225847b1234e2b116d6e3a
Tauno Lainevool <taunlai@greeny.cs.tlu.ee>
2015-09-25 13:25:33 +0300

    valideerimine

--- login.php
+++ login.php
@@ -5,6 +5,11 @@
 
 $email_error="";
 $parool_error="";
+$first_name_error="";
+$last_name_error="";
+$user_error="";
+$password_error="";
+$email_error=";
 //kontrollin kas keegi vajutas nuppu
 if($_SERVER["REQUEST_METHOD"]=="POST"){
 
@@ -19,6 +24,14 @@ if(empty($_POST["Parool"])){
 $parool_error = "See väli on kohustuslik";
 }
 
+if(empty($_POST["first_name"])){
+$first_name_error = "See väli on kohustuslik";
+}
+
+if(empty($_POST ["last_name"])){
+	$last_name_error="See väli on kohustuslik";
+	
+}
 
 
 }
@@ -41,68 +54,19 @@ $parool_error = "See väli on kohustuslik";
 <input type="password" placeholder="Parool"><?php echo $parool_error; ?> <br><br>
 <input type="submit" value="Logi sisse" > <br><br>
 </form>
- 
-
-<?php
-
-    // *********************
-    // ** LOO KASUTAJA *****
-    // *********************
-    if(isset($_POST["create"])){
-
-			if ( empty($_POST["User"]) ) {
-				$create_user_error = "See väli on kohustuslik";
-			}else{
-				$create_email = cleanInput($_POST["create_email"]);
-			}
-
-			if ( empty($_POST["create_password"]) ) {
-				$create_password_error = "See väli on kohustuslik";
-			} else {
-				if(strlen($_POST["create_password"]) < 8) {
-					$create_password_error = "Peab olema vähemalt 8 tähemärki pikk!";
-				}else{
-					$create_password = cleanInput($_POST["create_password"]);
-				}
-			}
-			
-			
-			
-			
-			
-
-			if(	$create_email_error == "" && $create_password_error == ""){
-				echo "Võib kasutajat luua! Kasutajanimi on ".$create_email." ja parool on ".$create_password;
-      }
-			{
-				$password_hash = hash("sha512", $create_password);
-				echo "<br>";
-				echo $password_hash;
-				
-				$stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?, ?)");
-				
-				//echo $mysqli->error;
-				//echo $stmt->error;
-				//asendame ? märgid muutujate väärtuste
-				// ss - s tähendab string iga muutuja kohta
-				$stmt->bind_param("ss", $create_email, $password_hash);
-				$stmt->execute();
-				$stmt->close();
-			}
-    } // create if end
-?>
-
-
 
 <h2> Create User </h2>
- 
-<input text="Name" placeholder ="Name">
-<input text ="Lastname" placeholder="Lastname"><br><br>
-<input text="User" placeholder="User"><?php $user_error;?>
-<input text="Password" placeholder="Password"><?php $password_error?><br><br>
-<input text="Email" placeholder="Email"><?php $emial_error?>
+
+
+<input name="first_name" type="text" placeholder ="Name"><?php echo $first_name_error; ?><br><br>
+<input name ="Lastname" type="text " placeholder="Lastname"><?php echo $last_name_error; ?><br><br>
+<input name="User" type="text" placeholder="User"><?php echo $user_error;?><br><br>
+<input name="Password" type="text" placeholder="Password"><?php echo $password_error?><br><br>
+<input name="Email" type="text" placeholder="Email"><?php echo $email_error?><br><br>
 <input type="submit" value="Submit"><br><br>
  Idee kirjeldus. Lisan lehele Logimis ja registeerimis vormid, peale mida lisan lehti juurde ja mõned pildid etc.
+
+
 
 
 </body>
