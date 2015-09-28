3723d8b79b4bb34fd04a342eefca735f84a6239e
Tauno Lainevool <taunlai@greeny.cs.tlu.ee>
2015-09-28 12:59:02 +0300

    error

--- login.php
+++ login.php
@@ -5,40 +5,19 @@
 
 $email_error="";
 $parool_error="";
-$first_name_error="";
-$last_name_error="";
-$user_error="";
-$password_error="";
-$email_error="";
+
 //kontrollin kas keegi vajutas nuppu
 if($_SERVER["REQUEST_METHOD"]=="POST"){
 
 echo "jah";
 
 	if(empty($_POST["email"])){
-$email_error = "See väli on kohustuslik";
+		$email_error = "See väli on kohustuslik";
 }
 
 
 	if(empty($_POST["Password"])){
 		$parool_error = "See väli on kohustuslik";
-		}
-
-	if(empty($_POST["first_name"])){
-		$first_name_error = "See väli on kohustuslik";
-		}
-
-if(empty($_POST ["last_name"])){
-	$last_name_error="See väli on kohustuslik";
-}	
-if(empty($_POST ["user_name"])){
-	$user_name_error="See väli on kohustuslik";
-}
-if(empty($_POST ["password"])){
-	$password_error="See väli on kohustuslik";
-}
-if(empty($_POST ["email"])){
-	$email_error="See väli on kohustuslik";
 }
 }
 ?>
@@ -61,7 +40,7 @@ if(empty($_POST ["email"])){
 <input type="submit" value="Logi sisse" > <br><br>
 </form>
 
-<h2> <a href="../1.kodutoo-II-ruhm/leht2">Create User </a> </h2>
+<h2> <a href="../1.kodutoo-II-ruhm/leht2">Not user? <br>Create User here!<br> </a> </h2>
 
 
 
