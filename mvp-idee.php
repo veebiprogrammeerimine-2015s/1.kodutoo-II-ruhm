ce17bceb64cb868ff3a27891dc6f196218a9052a
Vitali Maksimov <vitamak@greeny.cs.tlu.ee>
2016-01-13 17:14:41 +0200

    minu MVP-idee

---
+++ mvp-idee.php
@@ -0,0 +1,5 @@
+<h1>Minu MPV idee</h1>
+<p>Minu idee mvp-ks on teha sait kus sa voiks lisada ja vaatama kuulutused, naiteks tookuulutused ja nii edasi.
+<br>voib kaa rirjutada kommentaati igale kasutajale kes kirjutas kuulutus(enna logimisel).<br> </p>
+
+<body style="background-color:lightblue;"></body>
